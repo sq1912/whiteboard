a6783483847c7300e94995943a499c37f819c178
Philipp Kurz <phil.kurz@gmx.at>
2023-08-13 21:02:46 +0200

    added comment to requireemnt

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# todo activate environment using the command:
+# activate environment using the command:
 #   myenv\Scripts\activate
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hellp():
