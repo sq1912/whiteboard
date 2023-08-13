6471a3efc9ecba1f18ec49d944b3bca328dec7f9
Philipp Kurz <phil.kurz@gmx.at>
2023-08-13 22:16:09 +0200

    copied the output of ng build (from the dist folder) manually to flask's static folder, and produced an angulat_template.html to load the stuff.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # activate environment using the command:
 #   myenv\Scripts\activate
-
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+# Serve the Angular static files
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.root_path + '/static', filename)
 
+# Serve the Angular app template
 @app.route('/')
-def hellp():
-    return render_template('index.html')
+def index():
+    return render_template('angular_template.html')
+
+if __name__ == '__main__':
+    app.run()
