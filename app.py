11d8a9ebd3d5f4e6a0aa5bf7ef3ac745a28c4fc2
Philipp Kurz <phil.kurz@gmx.at>
2023-08-13 20:25:19 +0200

    added gitignore, html-template, requirements.txt

--- app.py
+++ app.py
@@ -1,54 +1,10 @@
+# todo activate environment using the command:
+#   myenv\Scripts\activate
 
-class Bier:
-  def __init__(self, bier_name: str, bier_type: str) -> None:
-    self.bier_name = bier_name
-    self.bier_type = bier_type
+from flask import Flask, render_template
 
-class BierMitHerkunft(Bier):
-  def __init__(self, bier_name: str, bier_type: str, herkunft: str) -> None:
-    super().__init__(bier_name, bier_type)
-    self.herkunft = herkunft
+app = Flask(__name__)
 
-def main():
-  """Das is Philipp's DocString!"""
-  print("ich bin main!")
-  b : Bier = Bier("Zipfer", "Marzen")
-  print(b.bier_name)
-
-  b2 : BierMitHerkunft = BierMitHerkunft("Gösser", "Martze", "Goess")
-  print(b2.bier_name)
-  print(b2.herkunft)
-
-def bla():
-  return "bla!" + "fasel"
-
-# this is my comment!
-main()
-print(bla())
-
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-
-print(len(thisdict.keys()))
-
-import datetime
-
-dd:datetime.datetime = datetime.datetime(2023,5, 6)
-print(dd)
-
-import json
-
-
-# some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
-print(y["age"]) 
-
-print(json.dumps((1,"a"))) 
+@app.route('/')
+def hellp():
+    return render_template('index.html')
